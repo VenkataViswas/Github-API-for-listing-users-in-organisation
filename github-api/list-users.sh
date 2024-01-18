a54206e094cf2866c10c6e22fd272f3a1cea2150
VenkataViswas <123818703+VenkataViswas@users.noreply.github.com>
2024-01-18 22:23:03 +0530

    Update list-users.sh

--- github-api/list-users.sh
+++ github-api/list-users.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+################
+
+# About : This shell script is used to list the users for a particular organisation 
+
+# input : It requires two inputs 1) username 2)token 
+
+# owner : Venkata Viswas
+
+# support : please contact iam_veeramalla for any problems or issues .
+
+###############
+
+helper()
+
 # GitHub API URL
 API_URL="https://api.github.com"
 
@@ -34,6 +48,14 @@ function list_users_with_read_access {
         echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
         echo "$collaborators"
     fi
+}
+
+function helper{
+expected_cmd_args=2
+if [ $# -ne $expected_cmd_args ]; then 
+echo "please execute the script with required cmd args"
+echo "asd"
+fi
 }
 
 # Main script
